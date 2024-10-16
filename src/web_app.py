eb7a92d35ee8b1286a3160db7642666a35631ef4
William Hart <william.hart@maths.ox.ac.uk>
2024-10-16 14:39:38 +0100

    Specify desired data location

--- src/web_app.py
+++ src/web_app.py
@@ -1,6 +1,8 @@
+import pathlib
 from climepi import climdata
 from climepi.app import get_app
 
+clim_dataset_example_base_dir = pathlib.Path(__file__).parents[1] / "data"
 clim_dataset_example_names = [
     name
     for name, details in climdata.EXAMPLES.items()
