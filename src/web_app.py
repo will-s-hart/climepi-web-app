1d0fe9ebf74228d9043da74a9f9bc4d12529bd89
William Hart <william.hart@maths.ox.ac.uk>
2024-10-30 10:14:27 +0000

    Log info about dask scheduler and try to catch if dask cluster not set up correctly

--- src/web_app.py
+++ src/web_app.py
@@ -1,10 +1,27 @@
+"""Python script defining the web app."""
+
+import logging
+import os
 import pathlib
 
 from climepi import climdata
 from climepi.app import get_app
 from dask.distributed import Client
 
-_ = Client()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+try:
+    dask_scheduler_address = os.environ["DASK_SCHEDULER_ADDRESS"]
+except KeyError as e:
+    raise ValueError("DASK_SCHEDULER_ADDRESS environment variable not set") from e
+client = Client(dask_scheduler_address)
+logger.info(
+    "Dask client connected to cluster at {%s} with {%s} workers",
+    dask_scheduler_address,
+    len(client.ncores()),
+)
+
 clim_dataset_example_base_dir = pathlib.Path(__file__).parents[1] / "data"
 clim_dataset_example_names = [
     name
