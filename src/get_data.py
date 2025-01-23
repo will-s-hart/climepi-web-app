f998433d92b4aceb7568d487d4106ea3959535a8
will-s-hart <william.hart@maths.ox.ac.uk>
2025-01-23 09:41:16 +0000

    Update example names from latest climepi version

--- src/get_data.py
+++ src/get_data.py
@@ -1,4 +1,4 @@
 from climepi import climdata
 
 if __name__ == "__main__":
-    climdata.get_example_dataset("isimip_cities", base_dir="/app/data")
+    climdata.get_example_dataset("isimip_cities_daily", base_dir="/app/data")
--- src/web_app.py
+++ src/web_app.py
@@ -2,16 +2,11 @@
 
 import pathlib
 
-from climepi import climdata
 from climepi.app import run_app
 
 if __name__ == "__main__":
     clim_dataset_example_base_dir = pathlib.Path(__file__).parents[1] / "data"
-    clim_dataset_example_names = [
-        name
-        for name, details in climdata.EXAMPLES.items()
-        if details.get("formatted_data_downloadable", False)
-    ]
+    clim_dataset_example_names = ["isimip_cities_daily"]
     run_app(
         clim_dataset_example_base_dir=clim_dataset_example_base_dir,
         clim_dataset_example_names=clim_dataset_example_names,
